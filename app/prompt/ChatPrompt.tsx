7afe9ced7d92f22db2e041bd5bda49a4c49b23c9
Connor Carey <connorcarey2005@gmail.com>
2024-10-20 07:28:56 -0700

    Changed message direction

--- app/prompt/ChatPrompt.tsx
+++ app/prompt/ChatPrompt.tsx
@@ -10,7 +10,7 @@ export default function ChatInput() {
 
     const handleSubmit = () => {
         if (value.trim()) {
-            setMessages((prevMessages) => [value, ...prevMessages]);
+            setMessages((prevMessages) => [...prevMessages, value]);
             setValue('');
         }
     };
