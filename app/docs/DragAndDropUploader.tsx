1a04641333d664b34d254b203a91c93779b7639e
Connor Carey <connorcarey2005@gmail.com>
2024-10-19 22:09:57 -0700

    Removed nextui boilerplate

--- app/docs/DragAndDropUploader.tsx
+++ app/docs/DragAndDropUploader.tsx
@@ -30,45 +30,50 @@ const DragAndDropUploader: React.FC = () => {
   };
 
   return (
-    <div>
-      <div
-        className="p-20 border-2 border-dashed border-gray-300 text-center cursor-pointer mb-5"
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-      >
-        <input
-          type="file"
-          multiple
-          accept="image/*"
-          className="hidden"
-          id="file-upload"
-          onChange={handleFileSelect}
-        />
-        <label htmlFor="file-upload" className="cursor-pointer">
-          <span className="text-gray-500">Drag & Drop images here, or click to upload</span>
-        </label>
+    <div className='min-h-screen'>
+      <div className='border-2 border-dashed rounded-md text-center'>
+        <p>Blank</p>
       </div>
-
-      {images.length > 0 && (
-        <div>
-          <span className="text-lg font-semibold">Uploaded Images:</span>
-          <div className="flex flex-wrap gap-4 mt-4">
-            {images.map((image, index) => (
-              <div key={index} className="p-2 border">
-                <Image
-                  src={image.preview}
-                  alt={image.name}
-                  width={100}
-                  height={100}
-                  className="object-cover"
-                />
-                <span className="block text-sm text-gray-500 mt-2">{image.name}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
     </div>
+    // <div>
+    //   <div
+    //     className="p-20 border-2 border-dashed border-gray-300 text-center cursor-pointer mb-5 rounded-md"
+    //     onDrop={handleDrop}
+    //     onDragOver={handleDragOver}
+    //   >
+    //     <input
+    //       type="file"
+    //       multiple
+    //       accept="image/*"
+    //       className="hidden"
+    //       id="file-upload"
+    //       onChange={handleFileSelect}
+    //     />
+    //     <label htmlFor="file-upload" className="cursor-pointer">
+    //       <span className="text-gray-500">Drag & Drop images here, or click to upload</span>
+    //     </label>
+    //   </div>
+
+    //   {images.length > 0 && (
+    //     <div>
+    //       <span className="text-lg font-semibold">Uploaded Images:</span>
+    //       <div className="flex flex-wrap gap-4 mt-4">
+    //         {images.map((image, index) => (
+    //           <div key={index} className="p-2 border">
+    //             <Image
+    //               src={image.preview}
+    //               alt={image.name}
+    //               width={100}
+    //               height={100}
+    //               className="object-cover"
+    //             />
+    //             <span className="block text-sm text-gray-500 mt-2">{image.name}</span>
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,7 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Look&nbsp;</span>
-        <span className={title({ color: "violet" })}>stylish&nbsp;</span>
-        <br />
-        <span className={title()}>
-          regardless of your
-        </span>
-        <br />
-        <span className={title()}>
-          fashion sense.
-        </span>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href="/docs"
-        >
-          <span className={subtitle()}>
-          Looksmaxx Now
-        </span>
-        </Link>
-        
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+    <div>
+      Blank.
+    </div>
   );
 }
